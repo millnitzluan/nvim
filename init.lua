7a453e9681795ee9dff5b913f7215c79f92b3f28
millnitzluan <luanmillnitz@gmail.com>
2021-07-16 10:33:08 -0300

    remove unused file-icons

--- init.lua
+++ init.lua
@@ -7,11 +7,6 @@ require "statusline"
 
 require("colorizer").setup()
 
--- -- neogit
--- local neogit = require('neogit')
-
--- neogit.setup {}
-
 -- lsp stuff
 require "nvim-lspconfig"
 require "compe-completion"
@@ -20,11 +15,11 @@ local cmd = vim.cmd
 local g = vim.g
 
 g.mapleader = " "
--- g.auto_save = 0
 
+g.ale_set_highlights = 0
 g.strip_whitespace_on_save = 1
 g.strip_whitespace_confirm = 0
-g.better_whitespace_enabled = 0
+g.better_whitespace_enabled = 1
 g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
 g.git_root = vim.fn.system("git rev-parse --git-dir"):gsub("/.git", "")
 
@@ -47,10 +42,11 @@ for _, setting in ipairs(SETTINGS) do
   vim.cmd(setting)
 end
 
+-- window split
 vim.api.nvim_set_keymap('n', '<Leader>v', '<C-w>v', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', '<Leader>h', '<C-w>s', { noremap = true, silent = false })
 
--- window navigation
+-- lsp bindings
 vim.api.nvim_set_keymap('n', 'gd', '[[<Cmd>lua vim.lsp.buf.definition()<CR>]]', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', 'gD', '[[<Cmd>lua vim.lsp.buf.declaration()<CR>]]', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', 'gr', '[[<Cmd>lua vim.lsp.buf.references()<CR>]]', { noremap = true, silent = false })
@@ -59,13 +55,13 @@ vim.api.nvim_set_keymap('n', '<C-s>', '[[<Cmd>lua vim.lsp.buf.signature_help()<C
 vim.api.nvim_set_keymap('n', '<C-n>', '[[<Cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]]', { noremap = true, silent = false })
 vim.api.nvim_set_keymap('n', '<C-p>', '[[<Cmd>lua vim.lsp.diagnostic.goto_next()<CR>]]', { noremap = true, silent = false })
 
-g.indentLine_enabled = 1
+g.indentLine_enabled = 0
 g.indent_blankline_char = "▏"
 
 g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard"}
 g.indent_blankline_buftype_exclude = {"terminal"}
 
-g.indent_blankline_show_trailing_blankline_indent = false
+g.indent_blankline_show_trailing_blankline_indent = true
 g.indent_blankline_show_first_indent_level = false
 
 require "treesitter-nvim"
@@ -73,7 +69,6 @@ require "mappings"
 
 require "telescope-nvim"
 require "nvimTree" -- file tree stuff
-require "file-icons"
 
 -- git signs , lsp symbols etc
 require "gitsigns-nvim"
